e749312cd7e00a836eaa7721c91bc3b974d1396d
Omar Valenzuela <ovalenzu@uci.edu>
2018-07-09 20:38:20 -0700

    added algorithm class and viewController for info (stats formerly)

---
+++ SORTa Helpful/InfoTabViewController.swift
@@ -0,0 +1,45 @@
+//
+//  InfoTabViewController.swift
+//  SORTa Helpful
+//
+//  Created by Omar Valenzuela on 7/4/18.
+//  Copyright © 2018 OB Devs. All rights reserved.
+//
+
+import UIKit
+
+class InfoTabViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let algorithms = ["classObjectsOfTypeAlgorithm"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
