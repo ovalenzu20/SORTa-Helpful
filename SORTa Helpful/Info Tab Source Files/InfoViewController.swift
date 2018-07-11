1bb901f577e48d4ff1f92e72f7f178789101526c
Omar Valenzuela <ovalenzu@uci.edu>
2018-07-11 16:19:49 -0700

    added ScrollViewController TODO: set dynamic height
    created label outlets in info tab
    TODO: sync label info in cell with algorithm data

--- SORTa Helpful/Info Tab Source Files/InfoViewController.swift
+++ SORTa Helpful/Info Tab Source Files/InfoViewController.swift
@@ -31,17 +31,19 @@ class InfoViewController: UIViewController, UITableViewDelegate, UITableViewData
         return algorithmCollection.count
     }
     
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let currentAlgorithm = algorithmCollection[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "algorithmCell")!
-        let text = currentAlgorithm.name
-        cell.textLabel?.text = text
         
-        let algorithmVC = storyboard?.instantiateViewController(withIdentifier: "AlgorithmViewController") as? AlgorithmViewController
-
-        algorithmVC?.algorithm = currentAlgorithm
+//        let text = currentAlgorithm.name
+//        cell.textLabel?.text = text
         
-        self.navigationController?.pushViewController(algorithmVC!, animated: true)
+//        let algorithmVC = storyboard?.instantiateViewController(withIdentifier: "AlgorithmViewController") as? AlgorithmViewController
+//
+//        algorithmVC?.algorithm = currentAlgorithm
+//
+//        self.navigationController?.pushViewController(algorithmVC!, animated: true)
 
         return cell 
     }
@@ -54,6 +56,8 @@ class InfoViewController: UIViewController, UITableViewDelegate, UITableViewData
         algorithmTableView.dataSource = self
         algorithmTableView.estimatedRowHeight = 2000
         algorithmTableView.rowHeight = UITableViewAutomaticDimension
+        self.algorithmTableView.rowHeight = CGFloat(2000/algorithmCollection.count)
+        
     }
 
     override func didReceiveMemoryWarning() {
