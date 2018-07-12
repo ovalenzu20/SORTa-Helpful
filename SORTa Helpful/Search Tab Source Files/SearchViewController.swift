b9463aa219ca5e3099193fc9360eba34fbde23f8
Brandon Teran <brandonteran@aim.com>
2018-07-12 13:57:07 -0700

    fixed merging conflicts and bug fixes with search view controller

--- SORTa Helpful/Search Tab Source Files/SearchViewController.swift
+++ SORTa Helpful/Search Tab Source Files/SearchViewController.swift
@@ -13,12 +13,20 @@ class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchCollectionView: UICollectionView!
     @IBOutlet weak var searchTextField: UITextField!
-    @IBOutlet weak var nameFilterButton: SearchFilterButton!
-    @IBOutlet weak var bestFilterButton: SearchFilterButton!
+    @IBOutlet weak var nameFilterButton:    SearchFilterButton!
+    @IBOutlet weak var bestFilterButton:    SearchFilterButton!
     @IBOutlet weak var averageFilterButton: SearchFilterButton!
-    @IBOutlet weak var worstFilterButton: SearchFilterButton!
+    @IBOutlet weak var worstFilterButton:   SearchFilterButton!
+    
     
     @IBAction func nameFilterActionButton(_ sender: SearchFilterButton) {
+        //        if nameFilterButton.state != .disabled {
+        //            nameFilterButton.setSelectedProperties()
+        //        }
+        //        else {
+        //            nameFilterButton.setUnselectedProperties()
+        //        }
+        
         nameFilterButton.setSelectedProperties()
         bestFilterButton.setUnselectedProperties()
         averageFilterButton.setUnselectedProperties()
@@ -32,6 +40,13 @@ class SearchViewController: UIViewController {
     }
     
     @IBAction func bestFilterActionButton(_ sender: SearchFilterButton) {
+        //        if bestFilterButton.state == .selected {
+        //            bestFilterButton.setSelectedProperties()
+        //        }
+        //        else {
+        //            bestFilterButton.setUnselectedProperties()
+        //        }
+        
         nameFilterButton.setUnselectedProperties()
         bestFilterButton.setSelectedProperties()
         averageFilterButton.setUnselectedProperties()
@@ -45,12 +60,12 @@ class SearchViewController: UIViewController {
     }
     
     @IBAction func averageFilterActionButton(_ sender: SearchFilterButton) {
-//        if averageFilterButton.state == .selected {
-//            averageFilterButton.setSelectedProperties()
-//        }
-//        else {
-//            averageFilterButton.setUnselectedProperties()
-//        }
+        //        if averageFilterButton.state == .selected {
+        //            averageFilterButton.setSelectedProperties()
+        //        }
+        //        else {
+        //            averageFilterButton.setUnselectedProperties()
+        //        }
         
         nameFilterButton.setUnselectedProperties()
         bestFilterButton.setUnselectedProperties()
@@ -65,12 +80,12 @@ class SearchViewController: UIViewController {
     }
     
     @IBAction func worstFilterActionButton(_ sender: SearchFilterButton) {
-//        if worstFilterButton.state == .selected {
-//            worstFilterButton.setSelectedProperties()
-//        }
-//        else {
-//            worstFilterButton.setUnselectedProperties()
-//        }
+        //        if worstFilterButton.state == .selected {
+        //            worstFilterButton.setSelectedProperties()
+        //        }
+        //        else {
+        //            worstFilterButton.setUnselectedProperties()
+        //        }
         
         nameFilterButton.setUnselectedProperties()
         bestFilterButton.setUnselectedProperties()
@@ -87,45 +102,24 @@ class SearchViewController: UIViewController {
     
     
     private let algorithmCollection : [Algorithm] = [
-        Algorithm(name: "Bubble Sort",     algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Selection Sort",  algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Insertion Sort",  algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Heap Sort",       algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Shell Sort",      algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Cocktail Sort",   algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Block Sort",      algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Merge Sort",      algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Quick Sort",      algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Cube Sort",       algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Binary Insertion Sort",  algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Counting Sort",   algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Radix Sort",      algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Spread Sort",     algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Bucket Sort",     algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
-        Algorithm(name: "Pigeonhole Sort", algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n²)", 10), averageCase: ("O(n²)", 10), worstCase: ("O(n²)", 10), memory: "O(n²)"),
+        Algorithm(name: "Bubble Sort",     algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(n²)", 10),     worstCase: ("O(n²)", 10),        memory: "O(1)"),
+        Algorithm(name: "Selection Sort",  algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n²)", 10),      averageCase: ("O(n²)", 10),     worstCase: ("O(n²)", 10),        memory: "O(1)"),
+        Algorithm(name: "Insertion Sort",  algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(n²)", 10),     worstCase: ("O(n²)", 10),        memory: "O(n²)"),
+        Algorithm(name: "Heap Sort",       algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(nlog(n))", 6), worstCase: ("O(nlog(n))", 6),    memory: "O(1)"),
+        Algorithm(name: "Cocktail Sort",   algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(n²)", 10),     worstCase: ("O(n²)", 10),        memory: "O(1)"),
+        Algorithm(name: "Block Sort",      algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(nlog(n))", 6), worstCase: ("O(nlog(n))", 6),    memory: "O(1)"),
+        Algorithm(name: "Merge Sort",      algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(nlog(n))", 6), averageCase: ("O(nlog(n))", 6), worstCase: ("O(nlog(n))", 6),    memory: "O(n)"),
+        Algorithm(name: "Quick Sort",      algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(nlog(n))", 6), averageCase: ("O(nlog(n))", 6), worstCase: ("O(n²)", 10),        memory: "O(log(n))"),
+        Algorithm(name: "Cube Sort",       algoClass: "Comparison",     algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(nlog(n))", 6), worstCase: ("O(nlog(n))", 6),    memory: "O(n)"),
+        Algorithm(name: "Counting Sort",   algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(n+r)", 2),     worstCase: ("O(n+r)", 2),        memory: "O(n+r)"),
+        Algorithm(name: "Radix Sort",      algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(n(k/d))", 3),  worstCase: ("O(n(k/d))", 3),     memory: "O(n+2ᵈ)"),
+        Algorithm(name: "Spread Sort",     algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(n(k/d))", 3),  worstCase: ("O(n((k/s)+d))", 4), memory: "O(n+2ᵈ)"),
+        Algorithm(name: "Bucket Sort",     algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(n+r)", 2),     worstCase: ("O(n+r)", 2),        memory: "O(n+r)"),
+        Algorithm(name: "Pigeonhole Sort", algoClass: "Non-comparison", algoType: "In-place", bestCase: ("O(n)", 1),       averageCase: ("O(n+2ᵏ)", 5),    worstCase: ("O(n+2ᵏ)", 5),       memory: "O(2ᵏ)"),
     ]
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        var algorithmViewController = segue.destination as! AlgorithmViewController
-    }
-    
-    
-    
-    let algorithmFilePath: String = "SupportingFiles/algorithmInfo.json"
-    
-    
-//    func loadJSON(filename fileName: String) -> [Algorithm]? {
-//        if let url = Bundle.main.url(forResource: fileName, withExtension: "json") {
-//            do {
-//                let data = try Data(contentsOf: url)
-//                let decoder = JSONDecoder()
-//                let jsonData = try decoder.decode(ResponseData.self, from: data)
-//                return jsonData.person
-//            } catch {
-//                print("error:\(error)")
-//            }
-//        }
-//        return nil
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        var algorithmViewController = segue.destination as! AlgorithmViewController
 //    }
     
     
@@ -137,6 +131,7 @@ class SearchViewController: UIViewController {
         
         searchCollectionView.delegate   = self
         searchCollectionView.dataSource = self
+        searchTextField.delegate = self
         
         let collectionWidth = (view.frame.size.width - 60) / 2
         let layout = searchCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
@@ -144,13 +139,17 @@ class SearchViewController: UIViewController {
         
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         navigationController?.navigationBar.shadowImage = UIImage()
+        
+        for algo in algorithmCollection {
+            searchedAlgorithms.append(algo)
+        }
     }
 }
 
 
-extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegate, UITextFieldDelegate {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cellData = algorithmCollection[indexPath.item]
+        let cellData = searchedAlgorithms[indexPath.item]
         let searchCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SearchCell", for: indexPath) as! SearchCell
 
         searchCell.setSearchButtonLabels(cellData: cellData)
@@ -195,11 +194,12 @@ extension SearchViewController: UICollectionViewDataSource, UICollectionViewDele
     
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return algorithmCollection.count
+        return searchedAlgorithms.count
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let currentAlgorithm = algorithmCollection[indexPath.row]
+        let currentAlgorithm = searchedAlgorithms[indexPath.item]
         
         let algorithmVC = storyboard?.instantiateViewController(withIdentifier: "AlgorithmViewController") as? AlgorithmViewController
         
@@ -207,7 +207,43 @@ extension SearchViewController: UICollectionViewDataSource, UICollectionViewDele
         
         self.navigationController?.pushViewController(algorithmVC!, animated: true)
     }
+    
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        searchTextField.resignFirstResponder()
+        searchTextField.text = ""
+        
+        searchedAlgorithms.removeAll()
+        
+        for algo in algorithmCollection {
+            searchedAlgorithms.append(algo)
+        }
+        
+        searchCollectionView.reloadData()
+        
+        return true
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (searchTextField.text?.count)! != 0 {
+            searchedAlgorithms.removeAll()
+            
+            for algo in algorithmCollection {
+                let range = algo.name.lowercased().range(of: searchTextField.text!, options: .caseInsensitive, range: nil, locale: nil)
+                
+                if range != nil {
+                    searchedAlgorithms.append(algo)
+                }
+            }
+        }
+        
+        searchCollectionView.reloadData()
+        
+        return true
+    }
 }
+
 
 
 
