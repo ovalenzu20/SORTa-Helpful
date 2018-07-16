8d5d8b4d32e56dadf607f7324220af4de01c9f25
Omar Valenzuela <ovalenzu@uci.edu>
2018-07-15 20:38:38 -0700

    created practice tab and flashcard button

--- SORTa Helpful/Search Tab Source Files/SearchViewController.swift
+++ SORTa Helpful/Search Tab Source Files/SearchViewController.swift
@@ -257,7 +257,7 @@ extension SearchViewController: UICollectionViewDataSource, UICollectionViewDele
            searchCell.setComplexityLabel(complexity: "")
         }
         
-        searchCell.backgroundColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+        //searchCell.backgroundColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
         return searchCell
     }
     
