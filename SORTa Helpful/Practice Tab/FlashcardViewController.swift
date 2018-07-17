ed7a55cd1477d76267db213d9163b78cfd4c996c
Omar Valenzuela <ovalenzu@uci.edu>
2018-07-17 14:15:16 -0700

    navigation bar controller configured in practice tab and make the tab bar black translucent

--- SORTa Helpful/Practice Tab/FlashcardViewController.swift
+++ SORTa Helpful/Practice Tab/FlashcardViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FlashcardViewController: UITabBarController {
+class FlashcardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
