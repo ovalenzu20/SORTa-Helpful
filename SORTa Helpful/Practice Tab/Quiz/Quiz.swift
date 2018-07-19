155a8799c5b927f39a2bd60c603d29d6cadcafc8
Omar Valenzuela <ovalenzu@uci.edu>
2018-07-18 19:57:31 -0700

    completed Quiz Class

--- SORTa Helpful/Practice Tab/Quiz/Quiz.swift
+++ SORTa Helpful/Practice Tab/Quiz/Quiz.swift
@@ -8,26 +8,77 @@
 
 import Foundation
 
-class Quiz : Test{
-//    var questions : [String: [String]] //the first position is the correcet answer
-    
-    var numberOfQuestions : Int
-    var numberOfQuestionsAnswered : Int
-    var numberOfQuestionsAnsweredCorrectly : Int
+enum questionOutOfBoundsError : Error {
+    case runtimeError(String)
+}
 
+
+class Question {
+    var question : String
+    var possibleAnswers: [String]
+    var correctAnswer: String
     
-    init (numberOfQuestions: Int, numberOfQuestionsAnswered : Int, numberOfQuestionsAnsweredCorrectly : Int, testName: String, testType: String)
+    init (question : String, possibleAnswers: [String], correctAnswer: String)
     {
-        self.numberOfQuestions = numberOfQuestions
-        self.numberOfQuestionsAnswered = numberOfQuestionsAnswered
-        self.numberOfQuestionsAnsweredCorrectly = numberOfQuestionsAnsweredCorrectly
-        
-        super.init(testName: testName, testType: testType)
-        
-        
+        self.question = question
+        self.possibleAnswers = possibleAnswers
+        self.correctAnswer = correctAnswer
     }
+}
 
+class Quiz {
+    private var questions = [Question]()
+    
+    private var quizName : String
+    private var numberOfQuestions = 0
+    private var numberOfQuestionsAnswered = 0
+    private var numberOfQuestionsAnsweredCorrectly = 0
+    private var numberOfQuestionsAnsweredIncorrectly = 0
+    
+    init (quizName: String)
+    {
+        self.quizName = quizName
+    }
+    
+    func addQuestion(question: Question){
+        questions.append(question)
+        numberOfQuestions += 1
+    }
+    
+    func answerQuestion(questionNumberToAnswer: Int, answer: String) throws -> Bool {
+        if questionNumberToAnswer < numberOfQuestions{
+            if questions[questionNumberToAnswer].correctAnswer == answer {
+                numberOfQuestionsAnswered += 1
+                numberOfQuestionsAnsweredCorrectly += 1
+                return true
+            }
+            
+            else {
+                numberOfQuestionsAnswered += 1
+                numberOfQuestionsAnsweredIncorrectly += 1
+                return false
+            }
+        }
+        //if questionNumberToAnswer > numberOfQuestions, throw exception
+        throw questionOutOfBoundsError.runtimeError("questionNumberToAnswer > numberOfQuestions")
+    }
+    
+    //calculateScore() is called when the user clicks "Calculate Score"
+    func calculateScore() -> Double
+    {
+        
+        let quizScore = Double(numberOfQuestionsAnsweredCorrectly) / Double(numberOfQuestionsAnswered)
+        let quizScoreRounded = round(quizScore, toDecimalPlaces: 2)
+        return quizScoreRounded
+    }
+    
+    func round(_ value: Double, toDecimalPlaces places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return Darwin.round(value * divisor) / divisor
+    }
+    
     
 }
+
 
 
