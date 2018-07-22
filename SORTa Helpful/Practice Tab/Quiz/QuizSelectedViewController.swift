8898ec55b1d84592a9df9fceadd6e2d13ebaea34
Omar Valenzuela <ovalenzu@uci.edu>
2018-07-22 15:21:12 -0700

    added "DONE WITH TEST" button

--- SORTa Helpful/Practice Tab/Quiz/QuizSelectedViewController.swift
+++ SORTa Helpful/Practice Tab/Quiz/QuizSelectedViewController.swift
@@ -25,32 +25,8 @@ class QuizSelectedViewController: UIViewController, UITableViewDelegate, UITable
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if indexPath.item > currentQuiz!.numberOfQuestions{
-//            //MAKE CALCULATE SCORE BUTTON
-//        }
         let cell = tableView.dequeueReusableCell(withIdentifier: "questionCell") as! QuizTableViewCell
-        
         cell.setQuestionButtonLabels(quiz: currentQuiz!, questionNumber: indexPath.item)
-
-//        let calculateScoreButton = UIButton()
-//        calculateScoreButton.frame = CGRect(x: self.view.frame.size.width - 60, y: 60, width: 50, height: 50)
-//        calculateScoreButton.setTitle("FINISH TEST", for: .normal)
-//        self.view.addSubview(calculateScoreButton)
-        
-//        calculateScoreButton.centerXAnchor.constraint(equalTo: cell.contentView.centerXAnchor).isActive = true
-//        calculateScoreButton.centerYAnchor.constraint(equalTo: cell.contentView.centerYAnchor).isActive = true
-//        calculateScoreButton.bottomAnchor.constraint(equalTo: cell.contentView.bottomAnchor, constant: 10).isActive = true
-//        calculateScoreButton.topAnchor.constraint(equalTo: cell.answersStackView.bottomAnchor, constant: 20).isActive = true
-//
-//        cell.answersStackView.bottomAnchor.constraint(equalTo: calculateScoreButton.topAnchor, constant: 20).isActive = true
-//
-//        calculateScoreButton.titleLabel?.text = "CALCULATE SCORE"
-//        calculateScoreButton.titleLabel?.font = UIFont(name: "Roboto", size: 20)
-//        calculateScoreButton.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
-//        calculateScoreButton.backgroundColor = #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)
-//        calculateScoreButton.widthAnchor.constraint(equalToConstant: 80)
-//        calculateScoreButton.heightAnchor.constraint(equalToConstant: 40.0)
-        
         return cell
     }
     
@@ -69,23 +45,14 @@ class QuizSelectedViewController: UIViewController, UITableViewDelegate, UITable
         
         self.title = currentQuiz!.quizName  //set testName
         self.navigationItem.setHidesBackButton(true, animated:true);
-
-//        quizName.text! = currentQuiz!.quizName  //set testName
-        
-        
         
         questionTableView.delegate = self
         questionTableView.dataSource = self
         questionTableView.rowHeight = 200
         
-        //addCalculateScoreButton()
     }
     
-    //goes into table view and adds to last cell
-    func addCalculateScoreButton(){
-        
-        
-    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
