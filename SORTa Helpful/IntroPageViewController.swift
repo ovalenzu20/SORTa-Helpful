f75bf46d4b529ff3eeef0699c30f7d53e895078c
Brandon Teran <brandonteran@aim.com>
2018-08-12 21:00:42 -0700

    Removed walkthrough buttons and changed walkthrough label constraints

--- SORTa Helpful/IntroPageViewController.swift
+++ SORTa Helpful/IntroPageViewController.swift
@@ -12,13 +12,12 @@ class IntroPageViewController: UIPageViewController {
     var introPageControl = UIPageControl()
 
     func configurePageControl() {
-        // The total number of pages that are available is based on how many available colors we have.
         introPageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width, height: 50))
         self.introPageControl.numberOfPages = introPages.count
         self.introPageControl.currentPage = 0
         self.introPageControl.tintColor = UIColor.black
-        self.introPageControl.pageIndicatorTintColor = UIColor.white
-        self.introPageControl.currentPageIndicatorTintColor = UIColor.black
+        self.introPageControl.pageIndicatorTintColor = UIColor.black
+        self.introPageControl.currentPageIndicatorTintColor = UIColor.white
         self.view.addSubview(introPageControl)
     }
     
@@ -55,18 +54,6 @@ class IntroPageViewController: UIPageViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 
